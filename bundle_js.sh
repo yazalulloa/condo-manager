3aef31ca06da089fc9ee130589292cdd37892849
Yazal Ulloa <yazal.ulloa@asesormasmovil.es>
2024-03-14 12:06:38 -0400

    before tw-elements update

--- bundle_js.sh
+++ bundle_js.sh
@@ -4,10 +4,10 @@ RES_DIR=$DIR/src/main/resources/META-INF/resources
 if [ -n "$1" ]
 then
   echo "Minify"
-  bunx build --minify  --outdir="$RES_DIR"/out "$RES_DIR"/js
+  bun build --minify  --outdir="$RES_DIR"/out "$RES_DIR"/js
 else
   echo "Watching"
-  bunx build --outdir="$RES_DIR"/out "$RES_DIR"/js  --watch
+  bun build --outdir="$RES_DIR"/out "$RES_DIR"/js  --watch
 fi
 
 
