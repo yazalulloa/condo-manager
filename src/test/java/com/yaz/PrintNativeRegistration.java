347181b377a976ce2b83d5a682a3d73ded79b621
Yazal Ulloa <yazal.ulloa@asesormasmovil.es>
2024-03-16 09:03:04 -0400

    remove db

--- src/test/java/com/yaz/PrintNativeRegistration.java
+++ src/test/java/com/yaz/PrintNativeRegistration.java
@@ -1,10 +1,14 @@
 package com.yaz;
 
+import com.yaz.reflection.NativeRegistration;
+import com.yaz.util.ReflectionUtil;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
@@ -13,18 +17,25 @@ public class PrintNativeRegistration {
 
   @Test
   void print() throws IOException {
+    final var classes = NativeRegistration.class.getAnnotation(RegisterForReflection.class)
+        .targets();
     final var serializationConfig = new JsonObject(
         Files.readString(Paths.get("native-config/native-image/serialization-config.json")));
     final var reflectConfig = new JsonArray(Files.readString(Paths.get("native-config/native-image/reflect-config.json")));
 
+
+
     final var set = Stream.concat(
-            serializationConfig.getJsonArray("types").stream()
-                .map(JsonObject.class::cast)
-                .map(j -> j.getString("name")),
-            reflectConfig.stream()
-                .map(JsonObject.class::cast)
-                .map(j -> j.getString("name"))
-                .filter(str -> str.startsWith("com.google."))
+            Arrays.stream(classes).map(Class::getName),
+            Stream.concat(
+                serializationConfig.getJsonArray("types").stream()
+                    .map(JsonObject.class::cast)
+                    .map(j -> j.getString("name")),
+                reflectConfig.stream()
+                    .map(JsonObject.class::cast)
+                    .map(j -> j.getString("name"))
+                    .filter(str -> str.startsWith("com.google."))
+            )
         )
         .map(str -> {
           final var i = str.indexOf('$');
@@ -35,6 +46,8 @@ public class PrintNativeRegistration {
           return str;
         })
         .map(s -> s + ".class")
+        .collect(Collectors.toSet())
+        .stream()
         .sorted()
         .collect(Collectors.joining(",\n"));
 
