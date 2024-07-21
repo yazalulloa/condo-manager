76625d4e1965f4151d74a2b21443bb143566a189
Yazal Ulloa <yazal.ulloa@asesormasmovil.es>
2024-07-21 14:47:43 -0400

    optimize js

--- sc/download_htmx_sse.sh
+++ sc/download_htmx_sse.sh
@@ -4,8 +4,8 @@ SSE_FILE="$DIR"/../frontend/js/sse.js
 DEST="$DIR"/../src/main/resources/META-INF/resources/out/js
 
 echo "Downloading htmx SSE extension to $SSE_FILE"
-curl -L "https://unpkg.com/htmx-ext-sse@2.2.0/sse.js" -o "$DEST"/sse.js
-#bun build "$SSE_FILE" --minify --target=browser --outfile "$DEST"/sse.js
+curl -L "https://unpkg.com/htmx-ext-sse@2.2.1/sse.js" -o "$SSE_FILE"
+bun build "$SSE_FILE" --minify --target=browser --outfile "$DEST"/sse.js
 #curl -L "https://unpkg.com/htmx.org/dist/ext/sse.js" -o "$SSE_FILE"
 
 
