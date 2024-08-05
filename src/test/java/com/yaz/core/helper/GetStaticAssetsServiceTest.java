8a7311cb8ca1dcb9df641010d373b6f07ad13b3a
Yazal Ulloa <yazal.ulloa@asesormasmovil.es>
2024-08-05 13:10:25 -0400

    fix prefetch

--- src/test/java/com/yaz/core/helper/GetStaticAssetsServiceTest.java
+++ src/test/java/com/yaz/core/helper/GetStaticAssetsServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import io.vertx.rxjava3.core.Vertx;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 class GetStaticAssetsServiceTest {
@@ -13,6 +14,20 @@ class GetStaticAssetsServiceTest {
     final var list = new GetStaticAssetsService(Vertx.vertx()).staticAssets().blockingGet();
 
     list.stream()
+        .filter(str -> !str.equals("/index.html"))
+        .map(str -> {
+
+          if (str.startsWith("/assets") || str.startsWith("/components")) {
+            return str;
+          }
+
+
+          if (str.endsWith("/index.html")) {
+            return "stc/" + str.substring(0, str.length() - 10);
+          }
+
+          return str;
+        })
         .map("<link rel=\"prefetch\" href=\"/%s\"/>"::formatted)
         .forEach(System.out::println);
   }
